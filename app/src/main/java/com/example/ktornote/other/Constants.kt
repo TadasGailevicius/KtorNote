2261ddea53c23427bd3f705318d8d814fade2a66
Tadas <tadasgailevicius@yahoo.com>
2021-01-26 01:18:25 +0200

    Add Constants to Constants.kt

--- app/src/main/java/com/example/ktornote/other/Constants.kt
+++ app/src/main/java/com/example/ktornote/other/Constants.kt
@@ -2,5 +2,11 @@ package com.example.ktornote.other
 
 object Constants {
 
+    const val DATABASE_NAME = "notes_db"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
+    const val BASE_URL = "http://192:168.8.103:8001"
+
     val IGNORE_AUTH_URLS = listOf("/login", "register")
 }
