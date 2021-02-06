2af92a8b11dee62924e14b67a90a89c97e0f1bf3
Tadas <tadasgailevicius@yahoo.com>
2021-02-06 22:10:33 +0200

    Move MainActivity.kt to ui package

--- app/src/main/java/com/example/ktornote/ui/MainActivity.kt
+++ app/src/main/java/com/example/ktornote/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.ktornote
+package com.example.ktornote.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ktornote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
