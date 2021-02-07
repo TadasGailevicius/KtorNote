c85d370c496f0b8f2b09e1f8539ff4670adbffb1
Tadas <tadasgailevicius@yahoo.com>
2021-02-07 23:55:07 +0200

    Update NoteRepository.kt Repository

--- app/src/main/java/com/example/ktornote/repositories/NoteRepository.kt
+++ app/src/main/java/com/example/ktornote/repositories/NoteRepository.kt
@@ -1,4 +1,30 @@
 package com.example.ktornote.repositories
 
-class NoteRepository {
+import android.app.Application
+import com.example.ktornote.data.local.NoteDao
+import com.example.ktornote.data.remote.NoteApi
+import com.example.ktornote.data.remote.requests.AccountRequest
+import com.example.ktornote.other.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(
+        private val noteDao: NoteDao,
+        private val noteApi: NoteApi,
+        private val context: Application
+) {
+    suspend fun register(email: String, password: String) = withContext(Dispatchers.IO) {
+        try {
+            val response = noteApi.register(AccountRequest(email, password))
+            if(response.isSuccessful && response.body()!!.successful) {
+                Resource.success(response.body()?.message)
+            } else {
+                Resource.error(response.body()?.message ?: response.message(), null)
+            }
+        } catch(e: Exception) {
+            Resource.error("Couldn't connect to the servers. Check your internet connection", null)
+        }
+    }
 }
