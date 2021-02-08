8b4074a58960c27dda69fef677014d839b999a31
Tadas <tadasgailevicius@yahoo.com>
2021-02-08 23:43:27 +0200

    Rename Constant in Constants.kt

--- app/src/main/java/com/example/ktornote/other/Constants.kt
+++ app/src/main/java/com/example/ktornote/other/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val DATABASE_NAME = "notes_db"
 
     const val KEY_LOGGED_IN_EMAIL = "KEY_LOGGED_IN_EMAIL"
-    const val KEY_LOGGED_IN_PASSWORD = "KEY_PASSWORD"
+    const val KEY_PASSWORD = "KEY_PASSWORD"
 
     const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
 
