cc238b1b71470d67c00b8d9adf6396ae8ab97c6c
Tadas <tadasgailevicius@yahoo.com>
2021-02-14 18:39:36 +0200

    Update notes classes

--- app/src/main/java/com/example/ktornote/ui/notes/NoteViewModel.kt
+++ app/src/main/java/com/example/ktornote/ui/notes/NoteViewModel.kt
@@ -1,9 +1,29 @@
 package com.example.ktornote.ui.notes
 
-import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.ktornote.data.local.entities.Note
+import com.example.ktornote.other.Event
+import com.example.ktornote.other.Resource
+import com.example.ktornote.repositories.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class NoteViewModel @ViewModelInject constructor(
-
+@HiltViewModel
+class NoteViewModel @Inject constructor(
+        private val repository: NoteRepository
 ) : ViewModel() {
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _allNotes = _forceUpdate.switchMap {
+        repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+    }.switchMap {
+        MutableLiveData(Event(it))
+    }
+
+    val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+    fun synchAllNotes() = _forceUpdate.postValue(true)
+
+
 }
--- app/src/main/java/com/example/ktornote/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/ktornote/ui/notes/NotesFragment.kt
@@ -1,15 +1,21 @@
 package com.example.ktornote.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
 import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ktornote.R
+import com.example.ktornote.adapters.NoteAdapter
 import com.example.ktornote.other.Constants.KEY_LOGGED_IN_EMAIL
 import com.example.ktornote.other.Constants.KEY_PASSWORD
 import com.example.ktornote.other.Constants.NO_EMAIL
 import com.example.ktornote.other.Constants.NO_PASSWORD
+import com.example.ktornote.other.Status
 import com.example.ktornote.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
@@ -18,8 +24,12 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
+    private val viewModel: NoteViewModel by viewModels()
+
     @Inject
     lateinit var sharedPref: SharedPreferences
+
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
@@ -28,9 +38,62 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+        setupRecyclerView()
+        subscribeToObservers()
+
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
+
         fabAddNote.setOnClickListener {
             findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
         }
+
+
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+            it?.let { event ->
+                val result = event.peekContent()
+                when(result.status){
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!!
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.ERROR -> {
+                        event.getContentIfNotHandled()?.let { errorResource ->
+                            errorResource.message?.let { message ->
+                                showSnackbar(message)
+                            }
+                        }
+
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = true
+                    }
+                }
+            }
+        })
+    }
+
+    private fun  setupRecyclerView() = rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+
     }
 
     private fun logout() {
