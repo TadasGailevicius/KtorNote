4dee92aa3ca0527038d0cc57408c4aa9d6d674bf
Tadas <tadasgailevicius@yahoo.com>
2021-03-01 23:47:24 +0200

    Update NoteDetailFragment.kt Fragment

--- app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailFragment.kt
@@ -1,7 +1,23 @@
 package com.example.ktornote.ui.notedetail
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.ktornote.R
 import com.example.ktornote.ui.BaseFragment
+import kotlinx.android.synthetic.main.fragment_note_detail.*
 
 class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
+
+    private val args: NoteDetailFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabEditNote.setOnClickListener {
+            findNavController().navigate(
+                    NoteDetailFragmentDirections.actionNoteDetailFragmentToAddEditNoteFragment(args.id)
+            )
+        }
+    }
 }
