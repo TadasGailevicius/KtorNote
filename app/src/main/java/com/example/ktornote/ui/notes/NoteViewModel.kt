0753e639006e86f80f3fc41ba9bacc0c90856144
Tadas <tadasgailevicius@yahoo.com>
2021-03-07 23:39:53 +0200

    Update notes classes

--- app/src/main/java/com/example/ktornote/ui/notes/NoteViewModel.kt
+++ app/src/main/java/com/example/ktornote/ui/notes/NoteViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.ktornote.ui.notes
 
 import androidx.lifecycle.*
+import com.example.ktornote.data.local.entities.LocallyDeletedNoteID
 import com.example.ktornote.data.local.entities.Note
 import com.example.ktornote.other.Event
 import com.example.ktornote.other.Resource
 import com.example.ktornote.repositories.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,6 +26,18 @@ class NoteViewModel @Inject constructor(
     val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
 
     fun synchAllNotes() = _forceUpdate.postValue(true)
+
+    fun insertNote(note: Note) = viewModelScope.launch {
+        repository.insertNote(note)
+    }
+
+    fun deleteNote(noteID: String) = viewModelScope.launch {
+        repository.deleteNote(noteID)
+    }
+
+    fun deleteLocallyDeletedNoteID(deletedNoteID: String) = viewModelScope.launch {
+        repository.deleteLocallyDeletedNoteID(deletedNoteID)
+    }
 
 
 }
