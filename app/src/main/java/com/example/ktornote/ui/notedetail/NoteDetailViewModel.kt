364580f4c2349651a28f166d370acb2fe73f8f3e
Tadas <tadasgailevicius@yahoo.com>
2021-04-11 21:45:21 +0300

    Update NoteDetailViewModel.kt ViewModel

--- app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailViewModel.kt
+++ app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.ktornote.ui.notedetail
 
 import androidx.lifecycle.ViewModel
+import com.example.ktornote.repositories.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteDetailViewModel @Inject constructor(
-
+    private val repository: NoteRepository
 ) : ViewModel() {
+
+    fun observeNoteById(noteID: String) = repository.observeNoteById(noteID)
 }
