9d0e0926781d1a210cfae84173f061bfd80cdc9e
Tadas <tadasgailevicius@yahoo.com>
2021-04-11 21:45:46 +0300

    Update NoteDetailFragment.kt Fragment

--- app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/example/ktornote/ui/notedetail/NoteDetailFragment.kt
@@ -2,22 +2,50 @@ package com.example.ktornote.ui.notedetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.ktornote.R
+import com.example.ktornote.data.local.entities.Note
 import com.example.ktornote.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import io.noties.markwon.Markwon
 import kotlinx.android.synthetic.main.fragment_note_detail.*
 
+@AndroidEntryPoint
 class NoteDetailFragment : BaseFragment(R.layout.fragment_note_detail) {
+
+    private val viewModel: NoteDetailViewModel by viewModels()
 
     private val args: NoteDetailFragmentArgs by navArgs()
 
+    private var curNote: Note? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
         fabEditNote.setOnClickListener {
             findNavController().navigate(
                     NoteDetailFragmentDirections.actionNoteDetailFragmentToAddEditNoteFragment(args.id)
             )
         }
+    }
+
+    private fun setMarkdownText(text: String) {
+        val markwon = Markwon.create(requireContext())
+        val markdown = markwon.toMarkdown(text)
+        markwon.setParsedMarkdown(tvNoteContent,markdown)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.observeNoteById(args.id).observe(viewLifecycleOwner, Observer {
+            it?.let { note ->
+                tvNoteTitle.text = note.title
+                setMarkdownText(note.content)
+                curNote = note
+            } ?: showSnackbar("Note not found")
+        })
+
     }
 }
